7d48e4b699af9ac5a0c45f66e24a136fed1f092f
JailedBird <zjc986812982@gmail.com>
2023-07-13 10:49:18 +0800

    feat: 优化Splitter处理

--- QALog/mainwindow.cpp
+++ QALog/mainwindow.cpp
@@ -1,11 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSplitter>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    // set tool bar
     ui->setupUi(this);
+
+    auto splitter = new QSplitter(Qt::Horizontal);
+
+    auto leftLayout = new QVBoxLayout(new QWidget(splitter));
+    auto button1 = new QPushButton(tr("Start001"));
+    auto button2 = new QPushButton(tr("Start002"));
+    auto button3 = new QPushButton(tr("Start003"));
+    leftLayout->addItem(new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding));
+    leftLayout->addWidget(button1);
+    leftLayout->addWidget(button2);
+    leftLayout->addWidget(button3);
+
+    auto rightLayout = new QVBoxLayout(new QWidget(splitter));
+    auto right001 = new QLabel(tr("Output"));
+    auto rightEdit = new QTextEdit () ;
+
+
+    leftLayout->addWidget(button1);
+    leftLayout->addWidget(button2);
+    leftLayout->addWidget(button3);
+
+    rightLayout->addWidget(right001);
+    rightLayout->addWidget(rightEdit);
+    splitter->setStretchFactor(0,1);
+    splitter->setStretchFactor(1,3);
+    if(centralWidget()!=nullptr){
+        delete centralWidget();
+    }
+    setCentralWidget(splitter);
+
+
 }
 
 MainWindow::~MainWindow()
